8266b05efafc920691c2a68530ceb3da157f107d
thatshowmafiaworks <shinehd56@gmail.com>
2022-10-11 19:11:00 +0300

    initial

---
+++ Folders/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Folders.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
